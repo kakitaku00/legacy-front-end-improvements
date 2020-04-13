b50fdab07db7540d790cfedb1b26eb6c6feea181
kakitaku00 <webtrakakinuma@gmail.com>
2020-04-13 17:45:07 +0900

    Vue ovservable

--- js/TodoApp.vue
+++ js/TodoApp.vue
@@ -21,10 +21,7 @@ import TodoEmpty from "./TodoEmpty"
 import NextTodo from "./NextTodo"
 import TodoCount from "./TodoCount"
 import AddTodoButton from "./AddTodoButton"
-import EventBus, {
-  ADD_TODO,
-  REMOVE_TODO
-} from './EventBus';
+import * as store from './ObservableStore'
 
 export default {
   components: {
@@ -34,43 +31,18 @@ export default {
     TodoCount,
     AddTodoButton
   },
-  data() {
-    return {
-      todoList: []
-    }
-  },
-  beforeMount() {
-    EventBus.$on(ADD_TODO, () => {
-      this.addTodo()
-    })
-    EventBus.$on(REMOVE_TODO, () => {
-      this.removeTodo();
-    })
-  },
+
   computed: {
-    todoCount() {
-      return this.todoList.length;
-    },
-    visibleTodoList() {
-      return this.todoCount > 0;
-    },
-    nextTodo() {
-      return this.todoCount > 0 ? this.todoList[0].todo : '(未登録)'
-    }
+    todoList: () => store.state.todoList,
+    todoCount: store.getters.todoCount,
+    visibleTodoList: store.getters.visibleTodoList,
+    nextTodo: store.getters.nextTodo
   },
+
   methods: {
-    addTodo() {
-      this.todoList.push({
-        key: new Date().getTime(),
-        todo: ''
-      });
-    },
-    removeTodo(index) {
-      this.todoList.splice(index, 1);
-    },
-    updateTodo({index, value}) {
-      this.todoList[index].todo = value;
-    }
+    addTodo: store.mutations.addTodo,
+    removeTodo: store.mutations.removeTodo,
+    updateTodo: store.mutations.updateTodo
   }
 }
 </script>
